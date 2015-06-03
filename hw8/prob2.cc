0b45c8c161dcc09ba5aefe78daa0fb6a00aa23a3
Richard Davis <richard.lee.davis@gmail.com>
2015-06-03 12:16:45 -0700

    Updated problem 2.

--- hw8/prob2.cc
+++ hw8/prob2.cc
@@ -7,16 +7,15 @@ int main() {
   string input = getInput();
 
   bool actualAnswer = (input == p);
+  if (actualAnswer) {
+    accept();
+  }
 
   for (int i = 0 to infinity) {
     for (each string c of length i) {
-      if (!imConvincedIsPasswordChecker(me, c)) {
-	if (actualAnswer) {
-	  accept();
-	} else {
-	  reject();
-	}
-      }
+      if (imConvincedIsPasswordChecker(me, c)) {                
+	accept(); // Once the certificate is found, accept any input
+      } 
     }
   }
 }
