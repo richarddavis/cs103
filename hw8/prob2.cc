a2f13ef0fb34c18ff8c48b5e03fd1603c5f451f0
Richard Davis <richard.lee.davis@gmail.com>
2015-06-03 02:07:19 -0700

    Adding source code for problem 2.

---
+++ hw8/prob2.cc
@@ -0,0 +1,24 @@
+bool imConvincedIsPasswordChecker(string program, string certificate) { 
+  /* ... some implementation ... */ 
+}
+
+int main() {
+  string me = mySource();
+  string input = getInput();
+
+  bool actualAnswer = (input == p);
+
+  for (int i = 0 to infinity) {
+    for (each string c of length i) {
+      if (imConvincedIsPasswordChecker(me, c)) {                
+	accept(); // Once the certificate is found, accept any input
+      } else {
+	if (actualAnswer) {
+	  accept();
+	} else {
+	  reject();
+	}
+      }
+    }
+  }
+}
